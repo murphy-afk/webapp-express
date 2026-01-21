1d7537d623e6e7f8d823765782cc770505958c0f
murphy-afk <bardarenoemi@gmail.com>
2026-01-21 09:55:41 +0100

    feat: static folder 'public'

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ import express from "express";
 import router from "./routers/movies.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import pageNotFound from "./middlewares/pageNotFound.js";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
-app.use(cors({
-  origin: "http://localhost:5173",
-}))
+app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  }),
+);
+
 
 app.use("/api/movies", router);
 
